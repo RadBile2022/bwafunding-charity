c889c04017bcf6a56c70fe68b20b792020b472b6
RadBile2022 <wahradar511@gmail.com>
2023-03-29 05:22:39 +0700

    1.1 Membuat komponen svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,15 @@
 <script>
-	export let name;
+	let title = "Charity";
+	setTimeout(function(){
+		title = "Donation"
+	},2000);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>Dalam penggunaan Continoous integation dapat memberikan trusted dalam deploys</p>
+	<h1>{title}</h1>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
 	h1 {
 		color: #ff3e00;
@@ -23,9 +18,5 @@
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
